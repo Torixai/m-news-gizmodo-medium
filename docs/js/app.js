9a76cf7fb476ab417e665b2ebbb27d65eb49fc68
Kisato Sakurai <kisato.sakurai@gmail.com>
2018-12-31 00:13:54 +0900

    disclosure made

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,23 @@
-$ = require("jQuery");
+var $ = require("jQuery");
 
 $(function() {
-  alert("ok");
+  var $menuIcon = $(".header-menu-icon");
+  var $disclosure = $(".header-disclosure");
+  var $closeIcon = $(".header-close-icon");
+
+  $menuIcon.on("click", () => {
+    $disclosure.slideDown(300);
+    $menuIcon.fadeOut(200);
+    setTimeout(() => {
+      $closeIcon.fadeIn(150);
+    }, 320);
+  });
+
+  $closeIcon.on("click", () => {
+    $disclosure.slideUp(300);
+    $closeIcon.fadeOut(200);
+    setTimeout(() => {
+      $menuIcon.fadeIn(150);
+    }, 320);
+  });
 });
